2db2b71391647ed82b2e17cf9fe42154543c9a21
Harsh Kandpal <harshksndpal1384@gmail.com>
2025-11-08 09:02:36 +0530

    Practiced interface implementation

--- main.java
+++ main.java
@@ -2,5 +2,32 @@ import java.util.*;
 
 interface Animal {
     void makeSound();
-    
+    void eat();
+    void sleep();
+}
+
+class Dog implements Animal {
+    @Override
+    public void makeSound(){
+        System.out.println("Woof!");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Dog is eating.");
+    }
+
+    @Override
+    public void sleep(){
+        System.out.println("Dog is sleeping.");
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        myDog.makeSound();
+        myDog.eat();
+        myDog.sleep();
+    }
 }
